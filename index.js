9e1ead2a528af6f6f6e09ee8a9bf844d4114b79f
Caroline Lyons <caroline.s.lyons@gmail.com>
2023-09-25 09:37:49 -0400

    commit tree c1c05b3b5ad9139b66f0ac70afae45bf03f20f7b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(list,occasion){
+    let completedCards = [];
+    for (let i = 0; i < list.length; i++){
+        let recipient = list[i];
+        completedCards.push(`Thank you, ${recipient}, for the wonderful ${occasion} gift!`)
+    }
+    return completedCards
+}
+
+function countDown(number){
+    let count = number;
+    while (count >= 0){
+        console.log(count);
+        count--
+    }
+}
